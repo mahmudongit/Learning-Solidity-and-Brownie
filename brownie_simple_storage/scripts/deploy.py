8bf0483f1533a1b3761ae212bd964de88296010a
Vane <mahmudonlinemoney@gmail.com>
2026-03-15 00:27:53 +0100

    Add files via upload

---
+++ brownie_simple_storage/scripts/deploy.py
@@ -0,0 +1,18 @@
+from brownie import accounts, config 
+from brownie import Tutorial
+
+def deploy_simple_storage():
+    account = accounts[0]
+    tutorial_storage = Tutorial.deploy({"from": account, "gas_price": "0"})
+    stored_value = tutorial_storage.retrieve()
+    print (stored_value)
+    transaction = tutorial_storage.store(20, {"from": account, "gas_price": "0"})
+    transaction.wait(1)
+    updated_stored_value = tutorial_storage.retrieve()
+    print (updated_stored_value)
+    #account = accounts.load("vanebuilds_")
+    #print (account)
+    #account = accounts.add(config["wallets"]["from_key"])
+    #print (account)
+def main():
+    deploy_simple_storage()
