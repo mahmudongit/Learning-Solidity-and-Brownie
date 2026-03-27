c7dd483aae18b26605fef826c5b284c8d398b962
Vane <mahmudonlinemoney@gmail.com>
2026-03-27 15:02:35 +0100

    Refactor FundMe contract to include interface

--- brownie_fund_me/contracts/FundMe.sol
+++ brownie_fund_me/contracts/FundMe.sol
@@ -1,7 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "../interfaces/AggregatorV3Interface.sol";
+interface AggregatorV3Interface {
+    function decimals() external view returns (uint8);
+    function description() external view returns (string memory);
+    function version() external view returns (uint256);
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
 
 contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
